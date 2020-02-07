e7d739541f96d940280449be21e8bce3c323e107
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2020-02-07 12:16:19 -0500

    remove  for ContractLike calls

--- mcd-pyk.py
+++ mcd-pyk.py
@@ -253,7 +253,7 @@ def argify(arg):
         newArg = 'UserLike(' + newArg + ')'
     if     newArg in ['Cat', 'Dai', 'End', 'Flap', 'Flop', 'Jug', 'Pot', 'Spot', 'Vat', 'Vow']
         or newArg.startswith('Flip_') or newArg.startswith('Gem_') or newArg.startswith('GemJoin_'):
-        newArg = newArg.lower().replace('_', '') + "Like(" + newArg + ')'
+        newArg = newArg.replace('_', '') + "Like(" + newArg + ')'
     if newArg in ['gold']:
         newArg = '"' + newArg + '"'
     return newArg
